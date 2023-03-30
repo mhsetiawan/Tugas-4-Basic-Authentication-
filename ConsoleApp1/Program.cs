718f4954f98e1b02c04304ee0c195f7f520b34a2
mhsetiawan <harissetiawan522@gmail.com>
2023-03-30 14:49:13 +0700

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,166 @@
+﻿using System.Text;
+using System.Timers;
+
+namespace ConsoleApp1;
+
+class Example01
+{
+    static void Main(string[] args)
+    {
+        int pilihMenu = 0, countUser = 1;
+        SortedList<int,User> user = new SortedList<int, User>();
+        string pesan;
+        Console.WriteLine(DateTime.Now);
+        do
+        {
+            Console.WriteLine("== BASIC AUTHENTICATION ==");
+            Console.WriteLine("1. Create User");
+            Console.WriteLine("2. Show User");
+            Console.WriteLine("3. Search User");
+            Console.WriteLine("4. Login User");
+            Console.WriteLine("5. Exit");
+            Console.Write("Input :");
+            pilihMenu = Convert.ToInt32(Console.ReadLine());
+            switch (pilihMenu)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("==CREATE USER==");
+                    User newUser = new User();
+                    newUser.InputNewUser(newUser);
+
+                    //Cek Username Sama
+                    if (user.Any(u => u.Value.Username.Contains(newUser.Username)))
+                    {
+                        newUser.Username = $"{newUser.FirstName[..2]}{newUser.LastName[..2]}{user.Count()}";
+                    }
+                    user.Add(countUser,newUser);
+                    countUser++;
+
+                    Console.WriteLine("Data User Berhasil Dibuat");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case 2:
+                    int pilihan = 0;
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("==SHOW USER==");
+                        foreach (var item in user)
+                        {
+                            Console.WriteLine("=============================================");
+                            Console.WriteLine("ID       :" + item.Key);
+                            item.Value.PrintUser();
+                            Console.WriteLine("=============================================");
+                        }
+                 
+                        Console.WriteLine("Menu");
+                        Console.WriteLine("1. Edit User");
+                        Console.WriteLine("2. Delete User");
+                        Console.WriteLine("3. Back");
+                        Console.WriteLine("Input :");
+                        pilihan = Convert.ToInt32(Console.ReadLine());
+                        switch (pilihan)
+                        {
+                            case 1:
+                                Console.Write("ID yang ingin diubah :");
+                                int idUbah = Convert.ToInt32(Console.ReadLine());
+                                //Ubah First Name
+                                Console.Write("First Name :");
+                                string newFname = Console.ReadLine();
+                                user[idUbah].FirstName = newFname;
+                                //Ubah Last Name
+                                Console.Write("Last Name :");
+                                string newLname = Console.ReadLine();
+                                user[idUbah].LastName = newLname;
+                                //Ubah Username
+                                user[idUbah].Username = $"{user[idUbah].FirstName[..2]}{user[idUbah].LastName[..2]}";
+                                //Ubah Password
+                                Console.Write("Password :");
+                                string newPass = Console.ReadLine();
+                                user[idUbah].Pass = newPass;                             
+
+                                Console.WriteLine($"User dengan id {idUbah} berhasil di edit");
+                                Console.ReadKey();
+                                break;
+                            case 2:
+                                Console.Write("ID yang ingin dihapus :");
+                                int idHapus = Convert.ToInt32(Console.ReadLine());
+                                user.Remove(idHapus);
+                                Console.WriteLine("Akun Berhasil dihapus");
+                                Console.ReadKey();
+                                break;
+                            case 3:
+                                Console.Clear();
+                                break;
+                            default:
+                                pesan = "Input Tidak Valid, Silahkan Pilih Ulang";
+                                Console.WriteLine(pesan);
+                                break;
+                        }
+                    } while (pilihan != 3);
+
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("==CARI AKUN==");
+                    string namaCari;
+                    Console.Write("Masukkan Nama :");
+                    namaCari = Console.ReadLine();
+                    var search = (from u in user where u.Value.FirstName.ToLower().Contains(namaCari.ToLower())
+                                  || u.Value.LastName.ToLower().Contains(namaCari.ToLower()) select u).ToList();
+
+                    if (search.Any())
+                    {
+                        foreach (var item in search)
+                        {
+                            Console.WriteLine("=============================================");
+                            Console.WriteLine("ID       :" + item.Key);
+                            item.Value.PrintUser();
+                            Console.WriteLine("=============================================");
+                        }
+                       
+                    }
+                    else {
+                        Console.WriteLine("==Akun Tidak Ditemukan==");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("==LOGIN==");
+                    Console.Write("USERNAME :");
+                    string username;
+                    username = Console.ReadLine();
+
+                    Console.Write("Password :");
+                    string pass;
+                    pass = Console.ReadLine();
+
+                    var login = (from u in user where u.Value.Username.Equals(username) && u.Value.Pass.Equals(pass) select u);
+
+                    if (login.Any())
+                    {
+                        Console.Write("Login Berhasil");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Login Gagal");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case 5:
+                    break;
+                default:
+                    pesan = "Input Tidak Valid, Silahkan Pilih Ulang";
+                    Console.WriteLine(pesan);
+                    break;
+            }
+        } while (pilihMenu != 5);
+
+
+    }
+}
---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1;
+
+public class User
+{
+    //Attribute
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Pass { get; set; }
+
+    public User()
+    {
+    }
+
+    public User(string firstName, string lastName, string pass)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Username = $"{firstName[..2]}{lastName[..2]}";
+        Pass = pass;
+    }
+
+    public void InputNewUser(User newUser) 
+    {
+        bool validPass = true;
+        Console.Write("First Name :");
+        newUser.FirstName = Console.ReadLine();
+        Console.Write("Last Name :");
+        newUser.LastName = Console.ReadLine();
+        do
+        {
+            Console.Write("Password :");
+            newUser.Pass = Console.ReadLine();
+            //cek syarat password
+            Regex r1 = new Regex("[0-9]");
+            Regex r2 = new Regex("[a-z]");
+            Regex r3 = new Regex("[A-Z]");
+            if (newUser.Pass.Length < 8 || !r1.IsMatch(newUser.Pass) || !r2.IsMatch(newUser.Pass) || !r3.IsMatch(newUser.Pass))
+            {
+                Console.WriteLine("Password harus terdiri dari minimal 8 karakter" +
+                    ", minimal satu huruf kapital, minimal satu huruf kecil, dan satu angka");
+                validPass = false;
+            }
+            else 
+            {
+                validPass = true;
+            }
+        } while (!validPass);
+        newUser.Username = $"{newUser.FirstName[..2]}{newUser.LastName[..2]}";
+    }
+
+    public void PrintUser() 
+    {
+        Console.WriteLine("Name     :" + FirstName + " " + LastName);
+        Console.WriteLine("Username :" + Username);
+        Console.WriteLine("Password :" + Pass);
+    }
+ 
+}
